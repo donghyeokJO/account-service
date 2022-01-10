fc085d0805e6e739f5af1bb04352ba98d067bc61
donghyeokJO <joeastgur@korea.ac.kr>
2022-01-10 14:50:54 +0900

    login localstorage

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API_URLS = {
-    USER_POST : '/user'
+    USER_POST: '/user',
+    USER_LOGIN: '/user/signin',
+    USER_ONE: '/user/one'
 }
 
 export default API_URLS
--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,7 +1,83 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import axios from 'axios';
+import API_URLS from "constants/api";
+import { pwdEncrypt } from "util/encrpyt";
+import { Link, useHistory } from "react-router-dom";
 
 export default function Login() {
+  let history = useHistory();
+  let token = window.localStorage.getItem("token");
+
+  if(token){
+    axios.get(
+      API_URLS.USER_ONE,
+      {
+        "headers": {
+          "Authorization": token
+        }
+      }
+    ).then(function(response){
+      if(response.data.result){
+        history.push("/landing")
+      }
+    }).catch(function(error){
+    })
+  }
+
+
+
+
+  const [USER_ID, SET_USER_ID] = useState("");
+  const [USER_PASSWORD, SET_USER_PASSWORD] = useState("");
+
+  const USER_ID_HANDLER = (event) => {
+    SET_USER_ID(event.currentTarget.value)
+  }
+
+  const USER_PASSWORD_HANDLER = (event) => {
+    SET_USER_PASSWORD(event.currentTarget.value)
+  }
+
+  const handleKeyPress = e => {
+    if(e.key === "Enter"){
+      onSubmit(e)
+    }
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if(
+      USER_ID === "" |
+      USER_PASSWORD === "" 
+    ) return alert('모든 항목을 입력해주세요.')
+    
+    axios.post(
+      API_URLS.USER_LOGIN,
+      {
+        "USER_ID": USER_ID,
+        "USER_PW": pwdEncrypt(USER_PASSWORD).encrypted_string
+      }
+    ).then(function(response){
+      console.log(response);
+      if(response.status === 200){
+        console.log(response.data.result.token)
+        window.localStorage.setItem('token', response.data.result.token);
+      
+        history.push('/landing');
+      }
+    })
+    .catch(function(error){
+      // console.log(error.response.status)
+      if(error.response.status === 404){
+        return alert('회원정보를 찾을 수 없습니다.')
+      }
+      else{
+        return alert("오류가 발생했습니다. 잠시 후 다시 시도해주세요.")
+      }
+    });
+    
+  }
+
   return (
     <>
       <div className="container mx-auto px-4 h-full">
@@ -21,9 +97,13 @@ export default function Login() {
                       ID
                     </label>
                     <input
-                      type="email"
+                      type="text"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="ID"
+                      name = "USER_ID"
+                      value = {USER_ID}
+                      onChange={USER_ID_HANDLER}
+                      onKeyPress={handleKeyPress}
                     />
                   </div>
 
@@ -32,18 +112,26 @@ export default function Login() {
                       className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                       htmlFor="grid-password"
                     >
-                      Password
+                      비밀번호
                     </label>
                     <input
                       type="password"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Password"
+                      placeholder="비밀번호"
+                      name = "USER_PASSWORD"
+                      value = {USER_PASSWORD}
+                      onChange={USER_PASSWORD_HANDLER}
+                      onKeyPress={handleKeyPress}
                     />
                   </div>
-                  <div>
+                  {/* <div>
                     <label className="inline-flex items-center cursor-pointer">
                       <input
-                        id="customCheckLogin"
+                        id="check"
+                        onChange={e => {
+                          changeHandler(e.currentTarget.checked, 'check');
+                        }}
+                        checked={checkedInputs.includes('check') ? true : false}
                         type="checkbox"
                         className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
                       />
@@ -51,12 +139,13 @@ export default function Login() {
                         자동 로그인
                       </span>
                     </label>
-                  </div>
+                  </div> */}
 
                   <div className="text-center mt-6">
                     <button
                       className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                       type="button"
+                      onClick={onSubmit}
                     >
                       로그인
                     </button>
